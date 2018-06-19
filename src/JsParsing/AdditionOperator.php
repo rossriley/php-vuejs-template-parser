dd1e0ac2154e6348f57322059dba1b52495e9dff
Ross Riley <riley.ross@gmail.com>
2018-06-19 14:20:27 +0800

    add support for addition operators

---
+++ src/JsParsing/AdditionOperator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WMDE\VueJsTemplating\JsParsing;
+
+class AdditionOperator implements ParsedExpression {
+
+    protected $left;
+    protected $right;
+
+    public function __construct($left, $right)
+    {
+        $this->left = $left;
+        $this->right = $right;
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return bool
+	 */
+	public function evaluate( array $data ): bool
+    {
+        return $this->left->evaluate($data) + $this->right->evaluate($data);
+	}
+
+}
